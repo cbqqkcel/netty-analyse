f1ce12848d7dadf9d35186f03c3c0b0491caa6e0
dmy <leiyangshi@vip.qq.com>
2013-06-16 22:19:05 +0800

    非线程安全的优先级队列

---
+++ test/com/dmycqq/test/concurrent/PriorityQueueTest.java
@@ -0,0 +1,30 @@
+package com.dmycqq.test.concurrent;
+
+import java.util.PriorityQueue;
+
+/**
+ * 非线程安全的优先级队列
+ * 线程安全 @see PriorityBlockingQueue
+ * @author DMY
+ *
+ */
+public class PriorityQueueTest {
+	public static void main(String[] args) {
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		pq.add(5);
+		pq.add(2);
+		pq.add(4);
+		pq.add(3);
+		pq.add(1);
+
+		for (Integer i : pq) {
+			System.out.print(i + " ");
+		}
+		
+		System.out.println();
+		
+		for (int i = pq.size(); i != 0; i--) {
+			System.out.print(pq.poll() + " ");
+		}
+	}
+}
