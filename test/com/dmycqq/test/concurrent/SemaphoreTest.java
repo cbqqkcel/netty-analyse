427f1720c1b541676120e08b9ff8be9920f62fe7
dmy <leiyangshi@vip.qq.com>
2013-06-16 22:44:34 +0800

    信号量

---
+++ test/com/dmycqq/test/concurrent/SemaphoreTest.java
@@ -0,0 +1,40 @@
+package com.dmycqq.test.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 信号量
+ * @author DMY
+ *
+ */
+public class SemaphoreTest {
+	public static void main(String[] args) throws Exception  {
+		// 线程池
+		ExecutorService exec = Executors.newCachedThreadPool();
+		// 只能个线程同时访问
+		final Semaphore semp = new Semaphore(1);
+		// 模拟个客户端访问
+		for (int index = 1; index < 21; index++) {
+			final int NO = index;
+			Runnable run = new Runnable() {
+				public void run() {
+					try {
+						// 获取许可
+						semp.acquire();
+						System.out.println("Accessing: " + NO);
+						Thread.sleep((long) (Math.random() * 1000));
+						// 访问完后，释放
+						System.out.println("Release: " + NO);
+						semp.release();
+					} catch (InterruptedException e) {
+					}
+				}
+			};
+			exec.execute(run);
+		}
+		// 退出线程池
+		exec.shutdown();
+	}
+}
